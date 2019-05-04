75871efc9262895c3f8330110e02fa0ef03e9f18
Isha Gupta <15913179+guptaisha5484@users.noreply.github.com>
2019-05-04 14:25:50 +1000

    Changed the App function to class

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: null
+    };
+  }
+
+  handleClick = () => {
+    //this.setState(send(this.state));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Intro to React Js workshop Wired Monash.</p>
+          <small>
+            You are running this app in <b>{process.env.NODE_ENV}</b> mode.
+          </small>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
